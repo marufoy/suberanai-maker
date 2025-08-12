c84b2e0d4863e0f90a4991f6a70dfee4921325b9
ryomaruyama <marusin618@gmail.com>
2025-08-12 15:21:19 +0900

    [change]デバッグトークンを追加

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,19 @@
 import 'modern-css-reset'; // ← リセットCSSを読み込み
 import '../styles/globals.css';
 import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
+import { useEffect } from 'react';
 import Script from 'next/script';
+import { ensureAppCheck } from '../lib/firebase';
 
 export default function App({ Component, pageProps }) {
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      ensureAppCheck();
+    }
+  }, []);
+
+
   return (
     <GoogleReCaptchaProvider
       reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_CLIENT_KEY}
