4d86c1a2f5652bc7aff976675438119a1ad8ddd8
ryomaruyama <marufoy0618@gmail.com>
2025-08-13 16:14:47 +0900

    App Checkと匿名認証を有効化し、AI連携を維持

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,23 @@
-// pages/_app.js（抜粋）
+// pages/_app.js
+import "modern-css-reset";
+import "../styles/globals.css";
 import { useEffect } from "react";
 import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 import { ensureAppCheck } from "../lib/firebase";
-import { ensureAnonAuth } from "../lib/auth";
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
-    ensureAppCheck();
-    ensureAnonAuth().catch(() => {/* noop */});
+    // クライアントでだけ App Check を初期化
+    if (typeof window !== "undefined") {
+      ensureAppCheck();
+    }
   }, []);
 
   return (
     <GoogleReCaptchaProvider
       reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_CLIENT_KEY}
       language="ja"
+      // recommended: scriptProps={{ async: true, defer: true }}
     >
       <Component {...pageProps} />
     </GoogleReCaptchaProvider>
